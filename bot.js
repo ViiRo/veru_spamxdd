38d83883931f6d091d5dbfa34ce7d891eae4cd4b
ViiRo <45905915+ViiRo@users.noreply.github.com>
2019-03-04 19:09:47 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("552155887356346379")
+setInterval(function() {
+channel.send(`I Love Myths Server <3 `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
